ef8eb9e5f4af03bdecab27faf7415781ea8d7fe4
Marcio Pinheiro <marciopinheiro452@gmail.com>
2023-03-09 23:00:11 -0300

    exerc11_aula3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,8 +2,16 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/' )
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () => {
+
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -13,7 +21,7 @@ context('Funcionalidade Login', () =>{
     })
 
     it('deve exibir uma mensagem de erro ou inserir usuário invalido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/' )
+
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
@@ -23,7 +31,7 @@ context('Funcionalidade Login', () =>{
 
     
     it('deve exibir uma mensagem de erro ou inserir ussario ou senha invalida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/' )
+
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
